e063cfd3d45f239982d34164f6d4b1be86c3f39c
ashwin <ashwinkn@hotmail.com>
2019-02-25 09:08:04 -0600

    Extended fixture and started bootstrap addon

--- src/js/__tests__/integration.js
+++ src/js/__tests__/integration.js
@@ -22,8 +22,19 @@ describe('# integration test', () => {
         expect(output).toMatchSnapshot();
     });
 
-    it('## should generate design and run react commands', () => {
+    it('## should generate design and run npm commands', () => {
         let output = execSync('./scripts/sgen-react.sh -d src/test/fixture/design.json -o testoutput').toString();
+        expect(output).toMatchSnapshot();
+        output = execSync('npm install', { cwd: 'testoutput' }).toString();
+        output = execSync('npm run lint', { cwd: 'testoutput' }).toString();
+        output = execSync('npm run build', { cwd: 'testoutput' }).toString();
+        output = execSync('npm test -- -u', { cwd: 'testoutput' }).toString();
+    });
+
+    it('## should generate design with bootstrap extensions and run npm commands', () => {
+        let output = execSync(
+            './scripts/sgen-react.sh -e templates/addons/bootstrap4 -d src/test/fixture/design.json -o testoutput'
+        ).toString();
         expect(output).toMatchSnapshot();
         output = execSync('npm install', { cwd: 'testoutput' }).toString();
         output = execSync('npm run lint', { cwd: 'testoutput' }).toString();
